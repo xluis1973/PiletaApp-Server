2b2c3e15abaf6eb50e05e5f7c3e06c824a3b30d7
Luis Javier Mercado <50953075+xluis1973@users.noreply.github.com>
2022-10-29 09:37:37 -0300

    servicioRest
    servicio de prueba

--- index.ts
+++ index.ts
@@ -1,12 +1,18 @@
 import Server from "./classes/server";
+import userRouter from './routes/usuario';
 
 const server=new Server();
 
-//Levantar express
+//Rutas de mi aplicación
+server.app.use('/user',userRouter);
 
+
+
+//Levantar express
 server.start(()=>{
 
     console.log(`Servidor corriendo ${ server.port }!!! `);
 });
 
-console.log("Se ha levantado el servidor");
+console.log("Se ha levantado el servidor. Con servicios");
+
---
+++ routes/usuario.ts
@@ -0,0 +1,11 @@
+import { Router,Request,Response } from "express";
+
+const userRouter= Router();
+
+userRouter.get('/prueba',(req:Request,resp:Response)=>{
+
+
+
+});
+
+export default userRouter;
